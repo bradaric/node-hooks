3af0d2876679ed5888a980de89ce35244a63a997
Alex Bradaric <alex@bradaric.com>
2016-04-07 16:06:18 +0200

    Refactors webhooks actions [#113206659]

--- server.js
+++ server.js
@@ -12,6 +12,65 @@
     console.log('UCN Hooks server listening on port ' + config.webhook.port + '...');
 
 
+    var _syncPartyDataTags = function(type, party_data, webhook_data) {
+        console.log('type', type);
+        console.log('party_data', party_data);
+        console.log('webhook_data', webhook_data);
+        console.log('GROUPINGS', webhook_data.merges.GROUPINGS);
+
+        if (typeof party_data.parties.person !== 'undefined' && party_data.parties.person.id) {
+            var person_id = party_data.parties.person.id;
+            var mailing_list = config.mailchimp.lists[webhook_data.list_id];
+
+            var note_action = '';
+            var segments = [];
+            switch (type) {
+                case 'subscribe':
+                    note_action = 'subscribed to';
+                    segments = _extractSegments(webhook_data);
+                    break;
+                case 'profile':
+                    note_action = 'updated their preferences for';
+                    segments = _extractSegments(webhook_data);
+                    break;
+                case 'unsubscribe':
+                    note_action = 'unsubscribed from';
+                    segments = [ 'unsubscribed' ];
+                    break;
+                case 'cleaned':
+                    note_action = 'been cleaned from';
+                    segments = [ 'cleaned' ];
+                    break;
+                default:
+                    break;
+            }
+
+            if (note_action) {
+                var note = { historyItem: { note: 'Contact has ' + note_action + ' mailing list "' + mailing_list.name + '"' } };
+                capsule.addHistoryFor('party', person_id, note, function(err, history_data) {
+                    console.log('addHistoryFor err', err);
+                    console.log('addHistoryFor data', history_data);
+                });
+            }
+
+            if (segments.length) {
+                var tag_name = mailing_list.tag;
+                if (tag_name && config.capsule.datatags[tag_name]) {
+                    var tags = { customFields: { customField: _segmentsToFields(segments, tag_name) } };
+                    console.log('tags', tags);
+                    capsule.setCustomFieldFor('party', person_id, tags, function(err, custom_field_data) {
+                        console.log('setCustomFieldFor err', err);
+                        console.log('setCustomFieldFor data', custom_field_data);
+                        capsule.setPartyTag(person_id, tag_name, function(err, party_tag_data) {
+                            console.log('setPartyTag err', err);
+                            console.log('setPartyTag data', party_tag_data);
+                        });
+                    });
+                }
+            }
+        }
+    };
+
     var _extractSegments = function(data) {
         var segments = [];
         if (typeof data.merges !== 'undefined' && data.merges.GROUPINGS) {
@@ -57,160 +116,53 @@
         console.log('error', error);
     });
 
-    webhook.on('subscribe', function (data, meta) {
-        var webhook_data = data;
+    webhook.on('subscribe', function (webhook_data, webhook_meta) {
         console.log(webhook_data.email + ' subscribed to your newsletter!');
-        console.log('webhook_data', webhook_data);
-        console.log('GROUPINGS', webhook_data.merges.GROUPINGS);
-        console.log('meta', meta);
-
-        capsule.personByEmail(data.email, function(err, data) {
+        capsule.personByEmail(webhook_data.email, function(err, party_data) {
             console.log('personByEmail err', err);
-            console.log('personByEmail data', data);
-            if (typeof data.parties.person !== 'undefined' && data.parties.person.id) {
-                var person_id = data.parties.person.id;
-                var mailing_list = config.mailchimp.lists[webhook_data.list_id];
-
-                var note = { historyItem: { note: 'Contact has subscribed to mailing list "' + mailing_list.name + '"' } };
-                capsule.addHistoryFor('party', person_id, note, function(err, data) {
-                    console.log('addHistoryFor err', err);
-                    console.log('addHistoryFor data', data);
-                });
-
-                var tag_name = mailing_list.tag;
-                if (tag_name && config.capsule.datatags[tag_name]) {
-                    var tags = { customFields: { customField: _segmentsToFields(_extractSegments(webhook_data), tag_name) } };
-                    console.log('tags', tags);
-                    capsule.setCustomFieldFor('party', person_id, tags, function(err, data) {
-                        console.log('setCustomFieldFor err', err);
-                        console.log('setCustomFieldFor data', data);
-                        capsule.setPartyTag(person_id, tag_name, function(err, data) {
-                            console.log('setPartyTag err', err);
-                            console.log('setPartyTag data', data);
-                        });
-                    });
-                }
+            if (!err) {
+                _syncPartyDataTags(webhook_meta.type, party_data, webhook_data);
             }
         });
     });
-    webhook.on('unsubscribe', function (data, meta) {
-        var webhook_data = data;
+    webhook.on('unsubscribe', function (webhook_data, webhook_meta) {
         console.log(webhook_data.email + ' unsubscribed from your newsletter!');
-        console.log('webhook_data', webhook_data);
-        console.log('GROUPINGS', webhook_data.merges.GROUPINGS);
-        console.log('meta', meta);
-
-        capsule.personByEmail(data.email, function(err, data) {
+        capsule.personByEmail(webhook_data.email, function(err, party_data) {
             console.log('personByEmail err', err);
-            console.log('personByEmail data', data);
-            if (typeof data.parties.person !== 'undefined' && data.parties.person.id) {
-                var person_id = data.parties.person.id;
-                var mailing_list = config.mailchimp.lists[webhook_data.list_id];
-
-                var note = { historyItem: { note: 'Contact has unsubscribed from mailing list "' + mailing_list.name + '"' } };
-                capsule.addHistoryFor('party', person_id, note, function(err, data) {
-                    console.log('addHistoryFor err', err);
-                    console.log('addHistoryFor data', data);
-                });
-
-                var tag_name = mailing_list.tag;
-                if (tag_name && config.capsule.datatags[tag_name]) {
-                    var tags = { customFields: { customField: _segmentsToFields(_extractSegments(null), tag_name) } };
-                    console.log('tags', tags);
-                    capsule.setCustomFieldFor('party', person_id, tags, function(err, data) {
-                        console.log('setCustomFieldFor err', err);
-                        console.log('setCustomFieldFor data', data);
-                        capsule.setPartyTag(person_id, tag_name, function(err, data) {
-                            console.log('setPartyTag err', err);
-                            console.log('setPartyTag data', data);
-                        });
-                    });
-                }
+            if (!err) {
+                _syncPartyDataTags(webhook_meta.type, party_data, webhook_data);
             }
         });
     });
 
-    webhook.on('profile', function (data, meta) {
-        var webhook_data = data;
+    webhook.on('profile', function (webhook_data, webhook_meta) {
         console.log(webhook_data.email + ' updated his profile!');
-        console.log('webhook_data', webhook_data);
-        console.log('GROUPINGS', webhook_data.merges.GROUPINGS);
-        console.log('meta', meta);
-
-        capsule.personByEmail(data.email, function(err, data) {
+        capsule.personByEmail(webhook_data.email, function(err, party_data) {
             console.log('personByEmail err', err);
-            console.log('personByEmail data', data);
-            if (typeof data.parties.person !== 'undefined' && data.parties.person.id) {
-                var person_id = data.parties.person.id;
-                var mailing_list = config.mailchimp.lists[webhook_data.list_id];
-
-                var note = { historyItem: { note: 'Contact has updated their preferences for mailing list "' + mailing_list.name + '"' } };
-                capsule.addHistoryFor('party', person_id, note, function(err, data) {
-                    console.log('addHistoryFor err', err);
-                    console.log('addHistoryFor data', data);
-                });
-
-                var tag_name = mailing_list.tag;
-                if (tag_name && config.capsule.datatags[tag_name]) {
-                    var tags = { customFields: { customField: _segmentsToFields([ 'unsubscribed' ], tag_name) } };
-                    console.log('tags', tags);
-                    capsule.setCustomFieldFor('party', person_id, tags, function(err, data) {
-                        console.log('setCustomFieldFor err', err);
-                        console.log('setCustomFieldFor data', data);
-                        capsule.setPartyTag(person_id, tag_name, function(err, data) {
-                            console.log('setPartyTag err', err);
-                            console.log('setPartyTag data', data);
-                        });
-                    });
-                }
+            if (!err) {
+                _syncPartyDataTags(webhook_meta.type, party_data, webhook_data);
             }
         });
     });
-    webhook.on('upemail', function (data, meta) {
-        console.log(data.email + ' updated his email address!');
-        console.log('data', data);
-        console.log('meta', meta);
+    webhook.on('upemail', function (webhook_data, webhook_meta) {
+        console.log(webhook_data.email + ' updated his email address!');
+        console.log('webhook_data', webhook_data);
+        console.log('webhook_meta', webhook_meta);
     });
-    webhook.on('cleaned', function (data, meta) {
-        var webhook_data = data;
+
+    webhook.on('cleaned', function (webhook_data, webhook_meta) {
         console.log(webhook_data.email + ' has been cleaned from your newsletter!');
-        console.log('webhook_data', webhook_data);
-        console.log('GROUPINGS', webhook_data.merges.GROUPINGS);
-        console.log('meta', meta);
-
-        capsule.personByEmail(data.email, function(err, data) {
+        capsule.personByEmail(webhook_data.email, function(err, party_data) {
             console.log('personByEmail err', err);
-            console.log('personByEmail data', data);
-            if (typeof data.parties.person !== 'undefined' && data.parties.person.id) {
-                var person_id = data.parties.person.id;
-                var mailing_list = config.mailchimp.lists[webhook_data.list_id];
-
-                var note = { historyItem: { note: 'Contact has been cleaned from mailing list "' + mailing_list.name + '"' } };
-                capsule.addHistoryFor('party', person_id, note, function(err, data) {
-                    console.log('addHistoryFor err', err);
-                    console.log('addHistoryFor data', data);
-                });
-
-                var tag_name = mailing_list.tag;
-                if (tag_name && config.capsule.datatags[tag_name]) {
-                    var tags = { customFields: { customField: _segmentsToFields([ 'cleaned' ], tag_name) } };
-                    console.log('tags', tags);
-                    capsule.setCustomFieldFor('party', person_id, tags, function(err, data) {
-                        console.log('setCustomFieldFor err', err);
-                        console.log('setCustomFieldFor data', data);
-                        capsule.setPartyTag(person_id, tag_name, function(err, data) {
-                            console.log('setPartyTag err', err);
-                            console.log('setPartyTag data', data);
-                        });
-                    });
-                }
+            if (!err) {
+                _syncPartyDataTags(webhook_meta.type, party_data, webhook_data);
             }
         });
     });
 
-    webhook.on('campaign', function (data, meta) {
+    webhook.on('campaign', function (webhook_data, webhook_meta) {
         console.log('status of campaign \'' + data.subject + '\' has been changed!');
-        console.log('data', data);
-        console.log('meta', meta);
+        console.log('webhook_data', webhook_data);
+        console.log('webhook_meta', webhook_meta);
     });
 })();
